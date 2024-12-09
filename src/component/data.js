0d7279a759cdcc3550b7222cb65557c05ba133ed
ilyas-ben-harou <ilyasbenharou@gmail.com>
2024-12-09 20:29:31 +0100

    my project

--- src/component/data.js
+++ src/component/data.js
@@ -1,52 +1,55 @@
 // data.js
-
+import produit1 from './images/produit1.jpeg';
+import produit2 from './images/produit2.jpeg';
+import produit3 from './images/produit3.jpeg';
+import produit4 from './images/produit4.jpeg';
 export const Data = [
     {
         "id": 1,
         title: 'Produit 1',
-        image: '/images/produit1.jpeg',
+        image: produit1,
         description: 'Description produit 1',
         price: 10.99,
     },
     {
         "id": 2,
         title: 'Produit 2',
-        image: '/images/produit2.jpeg',
+        image: produit2,
         description: 'Description produit 2',
         price: 10.99,
     },
     {
         "id": 3,
         title: 'Produit 3',
-        image: '/images/produit3.jpeg',
+        image: produit3,
         description: 'Description produit 3',
         price: 10.99,
     },
     {
         "id": 4,
         title: 'Produit 4',
-        image: '/images/produit4.jpeg',
+        image: produit4,
         description: 'Description produit 4',
         price: 10.99,
     },
     {
         "id": 5,
         title: 'Produit 5',
-        image: '/images/produit4.jpeg',
+        image: produit1,
         description: 'Description produit 4',
         price: 10.99,
     },
     {
         "id": 6,
         title: 'Produit 6',
-        image: '/images/produit1.jpeg',
+        image: produit2,
         description: 'Description produit 4',
         price: 10.99,
     },
     {
         "id": 7,
         title: 'Produit 7',
-        image: '/images/produit3.jpeg',
+        image: produit3,
         description: 'Description produit 4',
         price: 10.99,
     },
